9ecff3c12dd4b0e76142cf472dbdafb52f0abb3b
shumvlesu <sashumihin@gmail.com>
2020-06-08 16:13:31 +0300

    Initial commit

---
+++ app/src/main/java/com/shumikhin/beatbox/BeatBoxActivity.java
@@ -0,0 +1,14 @@
+package com.shumikhin.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeatBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.beat_box_activity);
+    }
+}
